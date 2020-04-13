09ff42e3059f7aadd541962ee52394a60e523d16
michalko <michal.lewandowski@gmail.com>
2020-04-13 20:13:49 +0200

    wrongly answered

--- src/main/java/com/brain1/core/rests/NextPostRest.java
+++ src/main/java/com/brain1/core/rests/NextPostRest.java
@@ -1,5 +1,7 @@
 package com.brain1.core.rests;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Random;
 import java.util.Set;
@@ -8,6 +10,7 @@ import javax.annotation.Resource;
 import javax.validation.constraints.NotNull;
 
 import com.brain1.core.services.NextPostService;
+import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,7 +34,7 @@ public class NextPostRest {
     static Random random = new Random();
 
     @Resource(name = "lastReadPosts")
-    com.brain1.core.rests.NextPostRest.MyConfiguration.LastReadPosts lastReadPosts;
+    MyConfiguration.UserTestMaintenance userTestMaintenance;
 
     @Autowired
     NextPostService nextPostService;
@@ -40,22 +43,70 @@ public class NextPostRest {
     @GetMapping(value = "/{topic}")
     @ResponseStatus(code = HttpStatus.OK)
     public @NotNull Integer getNextPost(@NotNull @PathVariable(value = "topic") final String topic,
-            @RequestParam("sub") final Optional<String> sub, @RequestParam("topRank") final int topRank) {
-        final var postsToOmit = Sets.union(lastReadPosts.getLastPostsIds(), Set.of(lastReadPosts.getLastPid()));
+            @RequestParam("sub") final Optional<String> sub, @RequestParam("topRank") final int topRank,
+            @RequestParam("lastCorrect") final boolean lastCorrect) {
+
+        if (lastCorrect) {
+            removeFromWrongAnswersIfPossible();
+        } else {
+            userTestMaintenance.getWrongAnswers().put(userTestMaintenance.getLastPid(),
+                    new PostStat(userTestMaintenance.getRealId(), 2));
+        }
+
+        Integer idToReturn = null;
+        if (timeToRepeatWronglyAnswered()) {
+            idToReturn = getRandomWrongAnswer();
+        } else {
+            idToReturn = getPostIdFromDB(topic, sub, topRank);
+        }
+        return idToReturn;
+
+    }
+
+    private Integer getRandomWrongAnswer() {
+        Random generator = new Random();
+        var values = userTestMaintenance.getWrongAnswers().values().toArray();
+        PostStat nextRandomPost = null;
+        do {
+            nextRandomPost = (PostStat) values[generator.nextInt(values.length)];
+        } while (nextRandomPost.realPostsInTopics == userTestMaintenance.getRealId());
+
+        userTestMaintenance.setRealId(nextRandomPost.realPostsInTopics);
+        userTestMaintenance.setAnswerCount(userTestMaintenance.getAnswerCount() + 1);
+        return nextRandomPost.realPostsInTopics;
+    }
+
+    private Integer getPostIdFromDB(final String topic, final Optional<String> sub, final int topRank) {
+        final var postsToOmit = Sets.union(userTestMaintenance.getLastPostsIds(),
+                Set.of(userTestMaintenance.getLastPid()));
         final var list = nextPostService.getNextPost(topic, sub, topRank, postsToOmit);
         final var listSize = list.size();
-        if (lastReadPosts.getPostsNum() == 0 || Math.abs(lastReadPosts.getTopRank() - topRank) > 10) {
-            lastReadPosts.setPostsNum(listSize);
-            lastReadPosts.setTopRank(topRank);
+        if (userTestMaintenance.getPostsNum() == 0 || Math.abs(userTestMaintenance.getTopRank() - topRank) > 10) {
+            userTestMaintenance.setPostsNum(listSize);
+            userTestMaintenance.setTopRank(topRank);
         }
 
         final var gauss = nextPostIndex(listSize);
         System.out.println(list);
         final var postToReturn = list.get(gauss);
-        lastReadPosts.addPost(postToReturn.getId());
-        lastReadPosts.setLastPid(postToReturn.getId());
 
+        userTestMaintenance.addPost(postToReturn.getId());
+        userTestMaintenance.setLastPid(postToReturn.getId());
+        userTestMaintenance.setRealId(postToReturn.getRealPostsInTopics());
+        userTestMaintenance.setAnswerCount(userTestMaintenance.getAnswerCount() + 1);
         return postToReturn.getRealPostsInTopics();
+    }
+
+    private boolean timeToRepeatWronglyAnswered() {
+        return userTestMaintenance.getWrongAnswers().size() > 1 && userTestMaintenance.getAnswerCount() % 2 == 0;
+    }
+
+    private void removeFromWrongAnswersIfPossible() {
+        if (userTestMaintenance.getWrongAnswers().containsKey(userTestMaintenance.getLastPid())) {
+            var postStat = userTestMaintenance.getWrongAnswers().get(userTestMaintenance.getLastPid());
+            PostStat newStat = new PostStat(postStat.realPostsInTopics, postStat.count - 1);
+            userTestMaintenance.getWrongAnswers().put(userTestMaintenance.getLastPid(), newStat);
+        }
     }
 
     private int nextPostIndex(final int listSize) {
@@ -63,28 +114,39 @@ public class NextPostRest {
         return gauss < 0 || gauss >= listSize ? listSize / 2 : gauss;
     }
 
+    record PostStat(int realPostsInTopics, int count) {
+    }
+
     @Configuration
     public class MyConfiguration {
         @Bean
         @SessionScope
-        public LastReadPosts lastReadPosts() {
-            return new LastReadPosts();
+        public UserTestMaintenance lastReadPosts() {
+            return new UserTestMaintenance();
         }
 
         @Component
-        public class LastReadPosts {
+        public class UserTestMaintenance {
             private final Set<String> lastPosts;
+            private final HashMap<String, PostStat> wrongAnswers;
             private int postsNum;
             private int topRank;
             private String lastPid = "0";
+            private int lastReadId = 0;
+            private int answerCount = 22;
 
-            LastReadPosts() {
+            UserTestMaintenance() {
                 lastPosts = Sets.newHashSet("1");
+                wrongAnswers = Maps.newHashMap();
             }
 
             void clear() {
                 lastPosts.clear();
                 postsNum = 0;
+            }
+
+            public HashMap<String, PostStat> getWrongAnswers() {
+                return wrongAnswers;
             }
 
             Set<String> getLastPostsIds() {
@@ -114,15 +176,30 @@ public class NextPostRest {
                 this.topRank = topRank;
             }
 
+            public int getRealId() {
+                return lastReadId;
+            }
+
+            public void setRealId(final int lastReadId) {
+                this.lastReadId = lastReadId;
+            }
+
             public String getLastPid() {
                 return lastPid;
             }
 
             public void setLastPid(final String lastPid) {
-
-                System.out.println("last posts ids 2: " + lastPid);
                 this.lastPid = lastPid;
             }
+
+            public int getAnswerCount() {
+                return answerCount;
+            }
+
+            public void setAnswerCount(int answerCount) {
+                this.answerCount = answerCount;
+            }
+
         }
 
     }
